e235f2479166cc9baccf9797cc53acd50bb0797c
VeronicaFontana <veronyc98@hotmail.it>
2023-12-06 22:40:36 +0100

    [FIX] API

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use app\Models\Project;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
